d3ef57093e9e22b8e89ba8c96e135eebf9742306
EvieQ01 <65006489+EvieQ01@users.noreply.github.com>
2022-10-22 13:19:04 +0800

    infoGAN(2phase)

--- infoGAN_room/main_infoGAN.py
+++ infoGAN_room/main_infoGAN.py
@@ -1,3 +1,4 @@
+import torch
 from copy import copy, deepcopy
 import random
 import numpy as np
@@ -5,21 +6,21 @@ import argparse
 import h5py
 import os
 import pdb
-import torch
 import math
 
 from torch import nn, optim
 from torch.autograd import Variable
 from torch.nn import functional as F
 import sys
+
+from models import Encoder
 sys.path.append('../IL')
 from boundary_utils import get_boundary_from_all_traj
 import grid_world as gw
 from load_expert_traj import Expert, ExpertHDF5
 
-from utils.logger import Logger, TensorboardXLogger
+# from utils.logger import Logger, TensorboardXLogger
 from dataset_utils import sub_traj_dataset, noise_sample
-import wandb
 import time
 from tqdm import trange
 from models import Generator, Discriminator, QHead
@@ -27,6 +28,7 @@ from torch.nn.utils.rnn import pad_packed_sequence
 from torch.nn.utils.rnn import pack_padded_sequence
 from plot_utils import plot_trajectory
 import seaborn as sns
+import wandb
 def main(args):
 
     # Create Logger
@@ -57,56 +59,50 @@ def main(args):
     criterionQ = nn.CrossEntropyLoss()
 
     # Initialise the network.
-    netG = Generator(input_size=args.c_dim+args.z_dim+len(expert.set_diff), max_len=args.max_len, output_size=len(expert.set_diff)).type(dtype)
+    # netG = Generator(input_size=args.c_dim+args.z_dim+len(expert.set_diff), max_len=args.max_len, output_size=len(expert.set_diff)).type(dtype)
+    netG = Generator(embed_size=args.embed_size).type(dtype)
     print(netG)
 
-    # x  -> 1
-    discriminator = Discriminator(input_size=len(expert.set_diff)).type(dtype)
+    # embed_x  -> 1
+    discriminator = Discriminator(input_size=args.embed_size).type(dtype)
     print(discriminator)
 
     # x  -> c
-    netQ = QHead(input_size=len(expert.set_diff), output_size=args.c_dim).type(dtype)
+    netQ = QHead(input_size=args.embed_size, output_size=args.c_dim).type(dtype)
     print(netQ)
 
-    optimD = optim.Adam([{'params': discriminator.parameters()}], lr=args.lr)#, betas=(args.beta1, args.beta2))
-    optimG = optim.Adam([{'params': netG.parameters()}, {'params': netQ.parameters()}], lr=args.lr)#, betas=(args.beta1, args.beta2))
+    # , {'params': encoder.s_embed.parameters()}
+    encoder = Encoder(vocab_size=len(expert.set_diff) + 1, embed_size=args.embed_size, c_size=args.c_dim).type(dtype)
+    optimD = optim.Adam([{'params': discriminator.parameters()}], lr=args.lr)#, weight_decay=1e-3)#, betas=(args.beta1, args.beta2))
+    optimG = optim.Adam([{'params': netG.parameters()}, {'params': netQ.parameters()}], lr=args.lr)#, weight_decay=1e-3)#, betas=(args.beta1, args.beta2))
     real_label = 1
     fake_label = 0
     for epoch in trange(args.num_epochs):
         # training
         for iter in range(dataset.size // args.batch_size):
-            # batch, mask = dataset.sample_padded_batch()
-            batch, boundary_index = dataset.sample_packed_batch()
-            # batch, boundary_index = dataset.sample_specific_batch(0)
-            # batch = batch.type(dtype)
-            batch = batch.to(device)
+            batch, boundary_index = dataset.sample_padded_batch() # (B, len)
+            noise_z, noise_c = noise_sample(dis_c_dim=args.c_dim, z_dim=len(expert.set_diff), batch_size=args.batch_size, dtype=dtype) # (B, c+z)
+            batch = batch.to(device) # (B, len)
+            batch_feat = encoder.s_embed(batch.type(label_dtype))
             # Updating discriminator and DHead
             optimD.zero_grad()
-            # Real data
-            label = torch.full((args.batch_size, ), real_label).type(dtype=dtype)
-            probs_real = torch.sigmoid(discriminator(batch)).view(-1) # (B, max_len, dim) -> (B, 1)
-            loss_real = criterionD(probs_real, label)
-            # Calculate gradients.
-            loss_real.backward()
 
             # Fake data
-            label.fill_(fake_label)
-            noise, idx = noise_sample(args.c_dim, z_dim=args.z_dim, batch_size=args.batch_size, dtype=dtype) # (B, c+z)
-            init_state = dataset.init_state_sample()
-            # fake_data = netG(x_0=init_state, z_c=noise) #TODO (B, max_len, dim)
-            fake_data = netG(x_0=batch, z_c=noise, teacher_force=args.teacher_force) #TODO (B, max_len, dim) [packed]
-            
-            # fake_data = torch.softmax(fake_data, dim=-1)
-            # detach!
-            fake_data_unpacked, lens_unpacked = pad_packed_sequence(fake_data, batch_first=True)
-            fake_data_detach = pack_padded_sequence(fake_data_unpacked.detach(), lengths=lens_unpacked, \
-                            batch_first=True, enforce_sorted=False)
-            
-            probs_fake = torch.sigmoid(discriminator(fake_data_detach)).view(-1)
-            # pdb.set_trace()
+            label = torch.full((args.batch_size, ), fake_label).type(dtype=dtype)
+            feat_z, feat_c = encoder(noise_z.type(label_dtype), noise_c.type(label_dtype)) # (B)(B) -> (B, embed) (B, embed)
+            fake_data_feat = netG(noise_z=feat_z, context=feat_c, teacher_force=args.teacher_force, x=batch_feat) #TODO (B, max_len, dim) [packed]
+            #TODO mask
+            probs_fake = torch.sigmoid(discriminator(fake_data_feat.detach())).view(-1)
             loss_fake = criterionD(probs_fake, label)
             # Calculate gradients.
-            loss_fake.backward()
+            loss_fake.backward(retain_graph=True)
+
+            # Real data
+            label.fill_(real_label)
+            probs_real = torch.sigmoid(discriminator(batch_feat)).view(-1) # discri(B, hidden) -> (B, 1)
+            loss_real = criterionD(probs_real, label)
+            # Calculate gradients.
+            loss_real.backward(retain_graph=True)
 
             # Net Loss for the discriminator
             D_loss = loss_real + loss_fake # probs are before softmax
@@ -116,16 +112,16 @@ def main(args):
             # Updating Generator and QHead
             optimG.zero_grad()
             # Fake data treated as real.
-            probs_fake = torch.sigmoid(discriminator(fake_data)).view(-1)
+            probs_fake = torch.sigmoid(discriminator(fake_data_feat)).view(-1)
             label.fill_(real_label)
             gen_loss = criterionD(probs_fake, label)
 
-            q_logits = netQ(fake_data) # change this from origin infoGAN
-            target = torch.tensor(idx).type(dtype=label_dtype)
+            q_logits = netQ(fake_data_feat) # change this from origin infoGAN
+            target = torch.tensor(noise_c).type(dtype=label_dtype)
             # Calculating loss for discrete latent code.
             #(B, c_dim), (B, c_dim)
             # pdb.set_trace()
-            posterior_loss = criterionQ(q_logits, target)
+            posterior_loss = criterionQ(q_logits, target.detach())
 
             # [extra distance constraints on c]
             view2 = torch.repeat_interleave(boundary_index[:, 0], args.batch_size) # B^2
@@ -133,17 +129,18 @@ def main(args):
             mask = torch.bitwise_or(torch.bitwise_and((view1 == view2 ).reshape((args.batch_size, -1)), boundary_index[:, 0] != -1), \
                     torch.bitwise_and((view1 == view2 ).reshape((args.batch_size, -1)), boundary_index[:, 1] != -1))
             mask = mask + mask.T
-            q_logits_detach = netQ(fake_data_detach) 
+            
+            q_logits_detach = netQ(fake_data_feat.detach()) 
             dist = torch.softmax(q_logits_detach, dim=-1).unsqueeze(0) - torch.softmax(q_logits_detach, dim=-1).unsqueeze(1) # B, B , dim_c
             # dist = q_logits_detach.unsqueeze(0) - q_logits_detach.unsqueeze(1) # B, B , dim_c
-            dist_loss = torch.sqrt(torch.mean(torch.sum(dist*dist, dim=-1)* mask / 2 ) ) 
+            dist_loss = torch.sqrt(torch.mean(torch.sum(dist*dist, dim=-1)* mask.type(dtype) / 2 ) ) 
             # Net loss for generator.
-            G_loss = gen_loss + posterior_loss * args.lambda_post + 1 / (dist_loss + 1e-5) * args.lambda_dist
+            G_loss = gen_loss * args.lambda_gen + posterior_loss * args.lambda_post + 1 / (dist_loss + 1e-5) * args.lambda_dist
             # Calculate gradients.
             # pdb.set_trace()
             G_loss.backward()
-            if epoch == 100:
-                pdb.set_trace()
+            # if epoch == 100:
+            #     pdb.set_trace()
             # Update parameters.
             # torch.nn.utils.clip_grad.clip_grad_norm_(netG.parameters(), max_norm=5.)
             # torch.nn.utils.clip_grad.clip_grad_norm_(discriminator.parameters(), max_norm=5.)
@@ -164,21 +161,23 @@ def main(args):
 
         # testing
         # dict of state and pred context
-        if epoch % 10 == 0:
-            state_and_pred_context = test_for_context(Qnet=netQ, expert=expert, boundary_list=boundary_list,\
-                                                        dataset=dataset, dtype=dtype, num_test_samples=10)
+        if (epoch  )% 10 == 0:
+            state_and_pred_context = test_for_context(Qnet=netQ, encoder=encoder.s_embed, expert=expert, boundary_list=boundary_list,\
+                                                        dataset=dataset, dtype=dtype, label_dtype=label_dtype,  num_test_samples=10)
 
             # plot
             fig_dir = os.path.join(os.path.dirname(args.results_pkl_path), 'visualize')
             os.makedirs(fig_dir,exist_ok='True')
-            plot_trajectory(state_and_pred_context,
-                            (15, 11),
+            
+            wandb_log_stamp = now if args.wandb else False
+            plot_trajectory(traj_context_data=state_and_pred_context,
+                            grid_size=(15, 11),
                             color_map=sns.color_palette("Blues_r"),
                             figsize=(6, 6),
                             obstacles=expert.obstacles,
-                            save_path=fig_dir)
+                            save_path=fig_dir, wandb_log_stamp=wandb_log_stamp, eposode=epoch)
 
-def test_for_context(Qnet:nn.Module, expert, boundary_list, dataset, dtype, num_test_samples=10):
+def test_for_context(Qnet:nn.Module, encoder:nn.Module, expert, boundary_list, dataset, dtype, label_dtype, num_test_samples=10):
     Qnet.eval()
     results = {'true_traj_state': [],'pred_context': []}
     # get sub_trajs for each traj
@@ -214,7 +213,8 @@ def test_for_context(Qnet:nn.Module, expert, boundary_list, dataset, dtype, num_
                 # get posterior c' = Q(x_{1:t}, c-1)
                 # store latent variables (logits or mu)
                 if t != 0:
-                    logits_q = F.softmax(Qnet(dataset.turn_xy_posi_to_onehot(x_var[0]).type(dtype).unsqueeze(0)), dim=-1)[0] #(B, c)
+                    x_feat = encoder(dataset.turn_xy_posi_to_onehot(x_var[0]).unsqueeze(0).type(label_dtype))
+                    logits_q = F.softmax(Qnet(x_feat), dim=-1)[0] #(B, c)
                     for _ in range(x_var.shape[-2]):
                         pred_context.append(logits_q.data.cpu().numpy())
                     if args.debug:
@@ -267,7 +267,7 @@ if __name__ == '__main__':
                         help='Action size for VAE.')
     parser.add_argument('--c_dim', type=int, default=10,
                         help='Context size for VAE.')
-    parser.add_argument('--z_dim', type=int, default=1,
+    parser.add_argument('--embed_size', type=int, default=10,
                         help='noise size for generator.')
 
     # lr
@@ -275,8 +275,9 @@ if __name__ == '__main__':
                         help='learning rate')
     parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam')
     parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam')
-    parser.add_argument('--lambda_post', type=float, default=.001, help='param for gen')
+    parser.add_argument('--lambda_post', type=float, default=.001, help='param for poset')
     parser.add_argument('--lambda_dist', type=float, default=.001, help='param for dist')
+    parser.add_argument('--lambda_gen', type=float, default=1., help='param for gen')
 
 
     # Use boundary
@@ -301,7 +302,7 @@ if __name__ == '__main__':
     parser.add_argument('--wandb', action='store_true', help='whether save on wandb')
     parser.add_argument('--teacher_force', action='store_true', help='whether use true state in RNN generator')
     args = parser.parse_args()
-    now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+    now = time.strftime(f"Gen{args.lambda_gen}_Post{args.lambda_post}_Dist{args.lambda_dist}"+"%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
     
     if args.wandb:
         wandb.init(project="infoGAN_grid_room", entity="evieq01")

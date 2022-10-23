26c764f139b092a9343d52bcc23579a6481e27a5
EvieQ01 <65006489+EvieQ01@users.noreply.github.com>
2022-10-23 10:03:56 +0800

    sh scrips

---
+++ infoGAN_room/run_separate_phase.sh
@@ -0,0 +1,3 @@
+python main_infoGAN.py --wandb --lambda_gen 0.01 --lambda_post 3.0 --lambda_dist 0.001 --cuda
+python main_infoGAN.py --wandb --lambda_gen 0.01 --lambda_post 3.0 --lambda_dist 0.01 --cuda
+python main_infoGAN.py --wandb --lambda_gen 0.01 --lambda_post 3.0 --lambda_dist 0.1 --cuda
